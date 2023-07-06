070932cd660224f4cd9c32cf76829c9de6468298
Miran Ahmad <222miran222@gmail.com>
2023-07-06 21:06:58 +0530

    api change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   firebase: {
-    projectId: 'learning-angular-firebas-ce0f8',
-    appId: '1:1087781786061:web:1978b04423fe6c718a3484',
-    storageBucket: 'learning-angular-firebas-ce0f8.appspot.com',
-    apiKey: 'AIzaSyDzYfBa-z0F4-G-OMrNr7MG16WstQ7Byzo',
-    authDomain: 'learning-angular-firebas-ce0f8.firebaseapp.com',
-    messagingSenderId: '1087781786061',
+  apiKey : "AIzaSyB6AWzJ59ordDlYv42Ldo5FTBW7jjtrmqA" , 
+  authDomain : "learning-angular-firebas-6c635.firebaseapp.com" , 
+  projectId : "learning-angular-firebas-6c635" , 
+  storageBucket : "learning-angular-firebas-6c635.appspot.com" , 
+  messagingSenderId : "540077908366" , 
+  appId : "1:540077908366:web:49ef194a46dcac27e8c4df"
   },
 };
